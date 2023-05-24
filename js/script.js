d8e93ce6a3a8d290b07e23370156c3aaf4b385b3
Sarah Thomas <sbade92@gmail.com>
2023-05-23 21:01:31 -0700

    Updated JS functions

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const lettersGuessed = [];
 
 const circleText = function(word){
     const dotLetters = [];
@@ -22,7 +23,34 @@ circleText(word);
 
 guessButton.addEventListener("click", function(e){
     e.preventDefault();
+    message.innerText = "";
     const entry = letterInput.value;
-    console.log(entry);
+    const goodEntry = validLetter(entry);
+    if (goodEntry){
+        makeGuess(entry);
+    }
     letterInput.value = "";
 });
+
+const validLetter = function(input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0){
+        message.innerText = "Please enter a value";
+    } else if (input.length > 1){
+        message.innerText = "Please enter only one letter";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a valid letter";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function(letter){
+    letter = letter.toUpperCase();
+    if (lettersGuessed.includes(letter)){
+        message.innerText = "You already guessed that letter. Try again!";
+    } else{
+        lettersGuessed.push(letter);
+        console.log(lettersGuessed);
+    }
+};
